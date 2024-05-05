f8bfe57bfe162650d31e91e0809e21303d7e3e17
1453n <1453n@DESKTOP-PHGFDVP>
2024-05-05 23:07:47 +0300

    Add project files.

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+int[] number = [1, 0, 5, 4, 8, 2, 6, 3, 7, 9];
+
+ InsortionSort(number);
+void InsortionSort<T>(T[] array) where T : IComparable
+{
+	for (int i = 1; i < array.Length; i++)
+	{
+		T key = array[i];
+		int j = 1 - i;
+
+		while (j > 0&& array[j].CompareTo(key)>0)
+		{
+			array[j+1]= array[j];
+			j--;
+
+		}
+		array[j+1] = key;
+
+	}
+
+
+}
